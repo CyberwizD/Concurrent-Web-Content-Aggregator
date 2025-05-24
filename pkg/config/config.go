e1a5380d3899db0c2b2420736fd364bf8437d867
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-05-24 03:29:38 +0100

    Remove unused Config struct and LoadConfig function from config package

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,12 +1,1 @@
 package config
-
-import "github.com/CyberwizD/RESTful-API-with-JWT-auth-and-RBAC/config"
-
-type Config struct {
-	App
-	sources
-}
-
-func LoadConfig(cg string, sc string) *config.Config {
-
-}
