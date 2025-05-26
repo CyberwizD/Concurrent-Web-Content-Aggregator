1c6a42a9ee8059c14224812934692b0850c3f90e
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-05-26 09:37:15 +0100

    update

--- internal/coordinator/pool.go
+++ internal/coordinator/pool.go
@@ -1,1 +1,30 @@
 package coordinator
+
+import (
+	"github.com/CyberwizD/Concurrent-Web-Content-Aggregator/internal/fetcher"
+	"github.com/CyberwizD/Concurrent-Web-Content-Aggregator/internal/parser"
+)
+
+type WorkerPool struct {
+	fetcher *fetcher.Fetcher
+	parser  *parser.Parser
+}
+
+func NewWorkerPool(maxWorker int, WorkerType string) *WorkerPool {
+	var fetcher *fetcher.Fetcher
+	var parser *parser.Parser
+
+	switch WorkerType {
+	case "fetcher":
+		fetcher = fetcher.New()
+	case "parser":
+		parser = parser.New()
+	default:
+		return nil, ErrInvalidWorkerType
+	}
+
+	return &WorkerPool{
+		fetcher: fetcher,
+		parser:  parser,
+	}
+}
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,1 +1,54 @@
 package parser
+
+import (
+	"fmt"
+)
+
+type Parser struct {
+	HtmlParser *HtmlParser
+	JsonParser *JsonParser
+	XmlParser  *XmlParser
+	RssParser  *RssParser
+}
+
+type HtmlParser struct {
+	// Add fields and methods specific to HTML parsing
+}
+
+type JsonParser struct {
+	// Add fields and methods specific to JSON parsing
+}
+
+type XmlParser struct {
+	// Add fields and methods specific to XML parsing
+}
+
+type RssParser struct {
+	// Add fields and methods specific to RSS parsing
+}
+
+// New creates a new Parser instance with the provided parsers.
+func New(htmlParser *HtmlParser, jsonParser *JsonParser, xmlParser *XmlParser, rssParser *RssParser) *Parser {
+	return &Parser{
+		HtmlParser: htmlParser,
+		JsonParser: jsonParser,
+		XmlParser:  xmlParser,
+		RssParser:  rssParser,
+	}
+}
+
+// Get returns a parser for the specified type.
+func Get(parserType string) (*Parser, error) {
+	switch parserType {
+	case "html":
+		return &Parser{HtmlParser: &HtmlParser{}}, nil
+	case "json":
+		return &Parser{JsonParser: &JsonParser{}}, nil
+	case "xml":
+		return &Parser{XmlParser: &XmlParser{}}, nil
+	case "rss":
+		return &Parser{RssParser: &RssParser{}}, nil
+	default:
+		return nil, fmt.Errorf("unknown parser type: %s", parserType)
+	}
+}
